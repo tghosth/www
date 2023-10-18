ffb5b28bdc31017a67ee3b2f8f1ba8135e258953
Josh Grossman (Bounce Security) <97975715+joshbouncesecurity@users.noreply.github.com>
2023-10-18 12:18:20 +0300

    Add scheduled task (#7)
    * Add scheduled task
    
    * Check cron every minute
    
    * Run cron daily
    
    * Add README

---
+++ netlify/functions/README.md
@@ -0,0 +1,8 @@
+# www-build-function
+
+This will run a daily build action in netlify.
+
+Do the following:
+1. Deploy this repository into netlify
+2. Set an environmental variable called `BUILD_HOOK` with the full URL of the build hook.
+3. Re-run the deploy to update the environmental variable.
---
+++ netlify/functions/scheduled-deploy.js
@@ -0,0 +1,19 @@
+import fetch from 'node-fetch'
+import { schedule } from '@netlify/functions'
+
+const BUILD_HOOK = process.env.BUILD_HOOK
+
+// Schedules the handler function
+const handler = schedule('0 1 * * *', async () => {
+  await fetch(BUILD_HOOK, {
+    method: 'POST'
+  }).then(response => {
+    console.log('Build hook response:', response)
+  })
+
+  return {
+    statusCode: 200
+  }
+})
+
+export { handler }
